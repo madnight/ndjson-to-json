af06ff9437ab613dbd21ad0f1e2f83c7a68dbb14
Fabian Beuke <mail@beuke.org>
2017-01-13 22:11:11 +0100

    add ndjson to json code

---
+++ index.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+fs.readFile(__dirname + "/" + process.argv[2], function(err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        var x = data.toString().split('\n').map(JSON.parse);
+        process.stdout.write(JSON.stringify(x));
+    }
+});
