70776866648ee59aed4c64a5c2a289444791988d
Fabian Beuke <mail@beuke.org>
2021-04-02 20:19:38 +0200

    add -o filename option

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ fs.readFile(process.argv[2], function(err, data) {
     if (err) {
         console.log(err);
     } else {
-        var x = data.toString().trim().split('\n').map(JSON.parse);
-        process.stdout.write(JSON.stringify(x));
+        var data = JSON.stringify(data.toString().trim().split('\n').map(JSON.parse));
+        if (process.argv[3] == "-o" && process.argv[4]) {
+           fs.writeFile(process.argv[4], data, function (err) {
+             if (err) return console.log(err);
+           });
+            return;
+        }
+        process.stdout.write(data);
     }
 });
